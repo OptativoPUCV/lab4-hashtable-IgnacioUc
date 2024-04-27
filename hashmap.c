9965dc7b489cff89735b67d82255c497274b5708
IgnacioUc <163429902+IgnacioUc@users.noreply.github.com>
2024-04-27 02:35:20 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -120,7 +120,15 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
+  if(map==NULL) return NULL;
+  while(map->current!=-1){
+    if(map->buckets[map->current]!=NULL && map->buckets[map->current]->key!=NULL{
+      return map->buckets[map->current];
+    }
+    map->current++;
+    
+  }
+  
     return NULL;
 }
 
